2efaf1f2f00248ef3ec31babbddb864f34b32f43
Asp105 <sanchez.pralv25@triana.salesianos.edu>
2026-01-08 10:52:02 +0100

    menu resta y división

--- src/proyectoColaborativo/Main.java
+++ src/proyectoColaborativo/Main.java
@@ -3,13 +3,21 @@ package proyectoColaborativo;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Hola, buenas este programa simula una calculadora.");
+		
 		double numero;
 		int menu;
 		do {
+			System.out.println("Elija una de las siguientes opciones: ");
+			System.out.println("3.Para restar");
+			System.out.println("4.Para dividir");
+			System.out.println("0.Para salir");
+			menu=Leer.
 			switch (menu) {
 			
 			}
 		} while (menu != 0);
+		
 	}
 
 }
