db5a6e20498df630ed3737b3af55edb8b115f735
Juan Ruiz <ruiz.cajua25@triana.salesianos.edu>
2026-01-08 10:51:14 +0100

    añadir la opcion de sumar y de multiplicar

--- src/proyectoColaborativo/Main.java
+++ src/proyectoColaborativo/Main.java
@@ -1,15 +1,33 @@
 package proyectoColaborativo;
 
+import utilidades.Leer;
+
 public class Main {
 
 	public static void main(String[] args) {
-		double numero;
+		double numero1, numero2;
 		int menu;
 		do {
+			System.out.println("1.Para sumar");
+			System.out.println("2.Para multiplicar");
+			menu = Leer.datoInt();
 			switch (menu) {
-			
+				case 1:
+					System.out.println("Introduzca el primer numero");
+					numero1 = Leer.datoDouble();
+					System.out.println("Introduzca el segundo numero");
+					numero2 = Leer.datoDouble();
+					System.out.printf("El resultado es %.4f", numero1 + numero2);
+					break;
+				case 2:
+					System.out.println("Introduzca el primer numero");
+					numero1 = Leer.datoDouble();
+					System.out.println("Introduzca el segundo numero");
+					numero2 = Leer.datoDouble();
+					System.out.printf("El resultado es %.4f", numero1 * numero2);
+					break;
 			}
 		} while (menu != 0);
+		System.out.println("Gracias por usar el programa");
 	}
-
 }
