d9236b3c610057cde4795e1c08e9d38997e28fce
Juan Ruiz <ruiz.cajua25@triana.salesianos.edu>
2026-01-08 10:37:04 +0100

    añadir el menu

--- src/proyectoColaborativo/Main.java
+++ src/proyectoColaborativo/Main.java
@@ -3,7 +3,13 @@ package proyectoColaborativo;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		double numero;
+		int menu;
+		do {
+			switch (menu) {
+			
+			}
+		} while (menu != 0);
 	}
 
 }
