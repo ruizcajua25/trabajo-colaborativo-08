fdd26730735e78be437d0f778e4ea03707e731f2
Asp105 <sanchez.pralv25@triana.salesianos.edu>
2026-01-08 10:57:27 +0100

    menu resta divison bien hecho

--- src/proyectoColaborativo/Main.java
+++ src/proyectoColaborativo/Main.java
@@ -1,20 +1,43 @@
 package proyectoColaborativo;
 
+import utilidades.Leer;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hola, buenas este programa simula una calculadora.");
 		
-		double numero;
+		double numero1, numero2;
 		int menu;
 		do {
 			System.out.println("Elija una de las siguientes opciones: ");
 			System.out.println("3.Para restar");
 			System.out.println("4.Para dividir");
 			System.out.println("0.Para salir");
-			menu=Leer.
+			System.out.println("Introduce el número de la opción elegida: ");
+			menu=Leer.datoInt();
 			switch (menu) {
-			
+				case 3:
+					System.out.println("Introduce el primer número: ");
+					numero1=Leer.datoDouble();
+					System.out.println("Introduce el segundo número: ");
+					numero2=Leer.datoDouble();
+					System.out.printf("El resultado es %.2f", numero1-numero2); 
+					break;
+					
+				case 4:
+					System.out.println("Introduce el primer número: ");
+					numero1=Leer.datoDouble();
+					System.out.println("Introduce el segundo número: ");
+					numero2=Leer.datoDouble();
+					System.out.printf("El resultado es %.2f", numero1/numero2); 
+					break;
+					
+				case 0:
+					break;
+					
+				default:
+					System.out.println("El número introducido es incorrecto, vuelva a intentarlo");
 			}
 		} while (menu != 0);
 		
